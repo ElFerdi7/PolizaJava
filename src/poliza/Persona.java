cd6198eaaa004d70536c4cccbd74bde8b8dfb9ef
a022583952e <a022583952e@s09-pc07>
2017-01-17 11:45:01 +0100

    Poliza

---
+++ src/poliza/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliza;
+
+/**
+ *
+ * @author a022583952e
+ */
+public class Persona {
+    
+    String nombre, direccion, dni;
+    int edad;
+    
+}
